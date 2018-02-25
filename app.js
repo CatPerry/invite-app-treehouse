b018d81c876ca79f40ed218ed6980adfbef9fbcf
Cat Perry <27353719+CatPerry@users.noreply.github.com>
2018-02-25 18:21:16 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,23 +1,55 @@
-//Now we'll write code to accept names into the form and place them in the unordered list ID invitedList
-//Now we'll add the names added to the input.value to the unordered list. Remember li elements are children of ul elements so we'll need to use appendChild im guessing.>> We'll need our handler to create a LIST ITEM, then we'll APPEND THAT ELEMENT TO THE UL. 
-//Now let's append a checkbox to 
 const form = document.getElementById('registrar');
 const input = form.querySelector('input');
+const divMain = document.querySelector('.main');
 const ul = document.getElementById('invitedList');
+const div = document.createElement('div');
+const filterLabel = document.createElement('label');
+const filterCheckbox = document.createElement('input');
+
+filterLabel.textContent = "Hide those who haven't responded";
+filterCheckbox.type = 'checkbox';
+div.appendChild(filterLabel);
+div.appendChild(filterCheckbox);
+divMain.insertBefore(div, ul);
+filterCheckbox.addEventListener('change', (e) => {
+	const isChecked = e.target.checked;
+	const lis = ul.children; 
+	if (isChecked) {
+		for (let i = 0; i < lis.length; i += 1) {
+			let li = lis[i];
+			if (li.className === 'responded') {
+				li.style.display = '';
+			} else {
+				li.style.display = 'none';
+			}
+		}
+	} else {
+		for (let i = 0; i < lis.length; i += 1) {
+			let li = lis[i];
+			li.style.display = '';
+		}
+	}
+});
+
 
 function createLi(text) {
 	let li = document.createElement('li');
-	li.textContent = text;
+	const span = document.createElement('span');
+	span.textContent = text;
+	li.appendChild(span);
 	const label = document.createElement('label');
 	label.textContent = 'Confirmed';
 	const checkbox = document.createElement('input');
 	checkbox.type = 'checkbox';
 	label.appendChild(checkbox);
 	li.appendChild(label);
-	const button = document.createElement('button');
-	button.type = 'button';
-	button.textContent = 'Remove';
-	li.appendChild(button);
+	const editButton = document.createElement('button');
+	editButton.textContent = 'edit';
+	const removeButton = document.createElement('button');
+	removeButton.type = 'button';
+	removeButton.textContent = 'remove';
+	li.appendChild(editButton);
+	li.appendChild(removeButton);
 	return li;
 }
 
@@ -28,16 +60,11 @@ form.addEventListener('submit', (e) => {
 	const li = createLi(text); 
 	ul.appendChild(li);
 });
-
-//Add an event handler to the checkbox. When the checkbox is cheked we’l add a class of RESPONDED  to the list item. Note i've also move the above UL const declaration to the global scope to be able to access it. 
+ 
 ul.addEventListener('change', (e) => {
-//Get a reference the checkbox itself
 	const checkbox = event.target;
-//once we have the value of the checkbox, which wll be true if the box is checked and false if it's not, we'll store it in a variable named checked. 
 	const checked = checkbox.checked;
-//Since we'll change the value of the CLASS once the checkbox is checked, we'll need a reference ot the list item. The list item is the checkbox's GRANDPARENT, because the label is the child of a the list item and the checkbox is the child of the label. So we can traverse to the list item by calling parentNode TWICE.
 	const listItem = checkbox.parentNode.parentNode; 
-//Set class name of responded (which we have a CSS Styling for) if checkbox is checked. And delete calssName if FALSE. We'll use an if statement for this.
 	if (checked) {
 		listItem.className = 'responded';
 	} else {
@@ -46,10 +73,27 @@ ul.addEventListener('change', (e) => {
 });
 
 ul.addEventListener('click', (e) => {
-//Remove listItem when button is clicked. Use the parent element NOT THE BUTTON, a syou started to do, since a lot of list elements may be added and delelted from this list. 
 	if (e.target.tagName === 'BUTTON') {
+		const button = e.target;
 		const li = e.target.parentNode;
 		const ul = li.parentNode;
+		if (button.textContent === 'remove') {
 		ul.removeChild(li);
+		} else if (button.textContent === 'edit') {
+			const span = li.firstElementChild;
+			const input = document.createElement('input');
+			input.type = 'text';
+			input.value = span.textContent;
+			li.insertBefore(input, span);
+			li.removeChild(span);
+			button.textContent = 'save';
+		} else if (button.textContent === 'save') {
+			const input = li.firstElementChild;
+			const span = document.createElement('span');
+			span.textContent = input.value;
+			li.insertBefore(span, input);
+			li.removeChild(input);
+			button.textContent = 'edit';
+			}
 	}
 });
